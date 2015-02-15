173e945f6f2874fdd77c889037261c58cfa4ff21
Ted Ying <yingted@gmail.com>
2015-02-14 21:09:36 -0500

    remove monkey patch

--- util.py
+++ util.py
@@ -8,17 +8,6 @@ import os
 sqlite3.register_adapter(pysrt.srttime.SubRipTime, str)
 sqlite3.register_converter('TIME', pysrt.srttime.SubRipTime.from_string)
 
-orig_sqlite3_connect = sqlite3.connect
-def sqlite3_connect(*args, **kwargs):
-	if 'detect_types' not in kwargs and 2 <= len(args):
-		args = list(args)
-		args[2] = sqlite3.PARSE_DECLTYPES
-		args = tuple(args)
-	else:
-		kwargs['detect_types'] = sqlite3.PARSE_DECLTYPES
-	return orig_sqlite3_connect(*args, **kwargs)
-sqlite3.connect = sqlite3_connect
-
 BLUEMIX_ROOT = os.getenv('BLUEMIX_ROOT')
 
 def get_paragraph_entities(paragraph):
