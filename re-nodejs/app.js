896bdba079921fde55d8d7826920df7809ca8946
VVCepheiA <oyevera@gmail.com>
2015-02-14 20:33:41 -0500

    wokring bluemix

---
+++ re-nodejs/app.js
@@ -0,0 +1,112 @@
+// app.js
+// This file contains the server side JavaScript code for your application.
+// This sample application uses express as web application framework (http://expressjs.com/),
+// and jade as template engine (http://jade-lang.com/).
+
+var express = require('express');
+var https = require('https');
+var url = require('url');
+var querystring = require('querystring');
+var xmlescape = require('xml-escape');
+
+// setup middleware
+var app = express();
+app.use(express.errorHandler());
+app.use(express.urlencoded()); // to support URL-encoded bodies
+app.use(app.router);
+
+app.use(express.static(__dirname + '/public')); //setup static public directory
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views'); //optional since express defaults to CWD/views
+
+// There are many useful environment variables available in process.env.
+// VCAP_APPLICATION contains useful information about a deployed application.
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+// TODO: Get application information and use it in your app.
+
+// defaults for dev outside bluemix
+var service_url = '<service_url>';
+var service_username = '<service_username>';
+var service_password = '<service_password>';
+
+// VCAP_SERVICES contains all the credentials of services bound to
+// this application. For details of its content, please refer to
+// the document or sample of each service.
+if (process.env.VCAP_SERVICES) {
+  console.log('Parsing VCAP_SERVICES');
+  var services = JSON.parse(process.env.VCAP_SERVICES);
+  //service name, check the VCAP_SERVICES in bluemix to get the name of the services you have
+  var service_name = 'relationship_extraction';
+  
+  if (services[service_name]) {
+    var svc = services[service_name][0].credentials;
+    service_url = svc.url;
+    service_username = svc.username;
+    service_password = svc.password;
+  } else {
+    console.log('The service '+service_name+' is not in the VCAP_SERVICES, did you forget to bind it?');
+  }
+
+} else {
+  console.log('No VCAP_SERVICES found in ENV, using defaults for local development');
+}
+
+console.log('service_url = ' + service_url);
+console.log('service_username = ' + service_username);
+console.log('service_password = ' + new Array(service_password.length).join("X"));
+
+var auth = 'Basic ' + new Buffer(service_username + ':' + service_password).toString('base64');
+
+// render index page
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+// Handle the form POST containing the text to identify with Watson and reply with the language
+app.post('/', function(req, res){
+
+  var parts = url.parse(service_url);
+
+  // create the request options from our form to POST to Watson
+  var options = { 
+    host: parts.hostname,
+    port: parts.port,
+    path: parts.pathname,
+    method: 'POST',
+    headers: {
+      'Content-Type'  :'application/x-www-form-urlencoded',
+      'X-synctimeout' : '30',
+      'Authorization' :  auth }
+  };
+
+  // Create a request to POST to Watson
+  var watson_req = https.request(options, function(result) {
+    result.setEncoding('utf-8');
+    var resp_string = '';
+
+    result.on("data", function(chunk) {
+      resp_string += chunk;
+    });
+
+    result.on('end', function() {
+      return res.render('index',{'xml':xmlescape(resp_string), 'text':req.body.txt})
+    })
+
+  });
+
+  watson_req.on('error', function(e) {
+    return res.render('index', {'error':e.message})
+  });
+
+  // Whire the form data to the service
+  watson_req.write(querystring.stringify(req.body));
+  watson_req.end();
+});
+
+
+// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+// The port on the DEA for communication with the application:
+var port = (process.env.VCAP_APP_PORT || 3000);
+// Start server
+app.listen(port, host);
